2a0118f991bcce1604ebc9e7df7134a4a0f11970
Icyyan <1581174331@qq.com>
2024-09-24 20:28:44 +0800

    用户注册模块修正 增加二次密码校验和用户名校验

--- src/main/java/com/wy0225/imbrlabel/service/impl/UserServiceImpl.java
+++ src/main/java/com/wy0225/imbrlabel/service/impl/UserServiceImpl.java
@@ -10,7 +10,7 @@ import com.wy0225.imbrlabel.pojo.DO.UserDO;
 import com.wy0225.imbrlabel.pojo.DTO.UserDTO;
 import com.wy0225.imbrlabel.pojo.DTO.UserRegisterDTO;
 import com.wy0225.imbrlabel.service.UserService;
-import org.springframework.beans.BeanUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
@@ -47,19 +47,27 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void register(UserRegisterDTO userRegisterDTO) {
-        userRegisterDTO.setPassword(DigestUtils.md5DigestAsHex(userRegisterDTO.getPassword().getBytes()));
-        UserDO userDO = new UserDO();
-        userDO=userMapper.getByUsername(userRegisterDTO.getUsername());
-        //查看用户名是否重复
-        if(userDO!=null){
-            throw new UserAlreadyExistException(UserConstant.USER_ALREADY_EXIST);
-        }
-        //查看两次密码是否相同
-        if(userRegisterDTO.getPassword().equals( userRegisterDTO.getRepassword())){
+        // 检查密码是否匹配
+        if (!userRegisterDTO.getPassword().equals(userRegisterDTO.getRepassword())) {
             throw new ReSetPasswordNotMatchException(UserConstant.RE_SET_PASSWORD_NOT_MATCH);
         }
-        BeanUtils.copyProperties(userDO, userRegisterDTO);
-        userDO.setNickname("新用户");//设置一下默认昵称，虽然好像没什么用
+
+        // 设置密码
+        String encryptedPassword = DigestUtils.md5DigestAsHex(userRegisterDTO.getPassword().getBytes());
+
+        // 检查用户名是否重复
+        UserDO existingUser = userMapper.getByUsername(userRegisterDTO.getUsername());
+        if (existingUser != null) {
+            throw new UserAlreadyExistException(UserConstant.USER_ALREADY_EXIST);
+        }
+
+        // 创建新的 UserDO 对象
+        UserDO userDO = new UserDO();
+        userDO.setUsername(userRegisterDTO.getUsername());
+        userDO.setPassword(encryptedPassword);
+        userDO.setNickname("新用户"); // 设置默认昵称
+
+        // 插入用户
         userMapper.insert(userDO);
     }
 }
