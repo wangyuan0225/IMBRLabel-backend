0966ac19dd8586a469b4da06aa11dba86f825186
zhaoyue <891444383@qq.com>
2024-11-01 14:24:02 +0800

    全自动调整标注方向

--- src/main/java/com/wy0225/imbrlabel/controller/AnnotationController.java
+++ src/main/java/com/wy0225/imbrlabel/controller/AnnotationController.java
@@ -12,13 +12,18 @@ import com.wy0225.imbrlabel.service.AnnotationService;
 import com.wy0225.imbrlabel.service.ImageService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+
+import static com.wy0225.imbrlabel.method.points.choicePointCounts;
 
 /**
  * @author wangy
@@ -32,6 +37,12 @@ public class AnnotationController {
 
     @Resource
     private ImageService imgService;
+
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
+    @Autowired
+    private ImageService imageService;
 
     /**
      * 添加标注模板
@@ -118,102 +129,20 @@ public class AnnotationController {
      * @param payload 请求体
      * @return 自动标注结果
      */
+    /**
+     * 自动标注
+     * @param payload 请求体
+     * @return 自动标注结果
+     */
     @PatchMapping("/auto")
     public Result<?> autoAnnotation(@RequestBody Map<String, Object> payload) {
         String annotations = (String) payload.get("annotations");
-        Long imageId = Long.parseLong((String) payload.get("imageId"));
-        List<Map<String, Object>> annotationsList;
+        Integer polygonsides = (Integer) payload.get("polygonSides");
 
-        // 解析现有的 annotation 字符串
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            String decodedAnnotations = java.net.URLDecoder.decode(annotations, StandardCharsets.UTF_8);//这里有报错但是不知道怎么改
-            annotationsList = objectMapper.readValue(decodedAnnotations, new TypeReference<>() {});
-        } catch (Exception e) {
-            return Result.error("Failed to parse annotations");
-        }
-
-        // 查找选中的矩形标签
-        Map<String, Object> selectedRectangle = null;
-        for (Map<String, Object> annotation : annotationsList) {
-            if (Boolean.TRUE.equals(annotation.get("active")) && Integer.valueOf(1).equals(annotation.get("type"))) {
-                selectedRectangle = annotation;
-                break;
-            }
-        }
-
-        // 检查是否找到选中的矩形标签
-        if (selectedRectangle == null) {
-            return Result.error("没有符合匹配标准的标签捏");
-        }
-
-        // 获取矩形标签的坐标
-        List<List<Integer>> coor = (List<List<Integer>>) selectedRectangle.get("coor");
-
-        // 获取当前图片路径
-        ImageDTO imageDTO = imgService.getImageById(imageId, BaseContext.getCurrentId());
-        String imagePath = imageDTO.getPath();
-
-        // 调用 Python 脚本并传递坐标
-        String txtFilePath = generateTxtFile(coor, imagePath);
-
-        //如果传给python是正常的则删除原来选中的矩形标签
-        annotationsList.remove(selectedRectangle);
-
-        // 从 TXT 文件中读取坐标
-        List<List<Integer>> coordinates = readCoordinatesFromTxt(txtFilePath);
-
-        // 生成新的多边形 annotation 并添加到原有集合
-        Map<String, Object> newAnnotation = new LinkedHashMap<>();
-        newAnnotation.put("label", "");  // 可能需要设置标签
-        newAnnotation.put("coor", coordinates);
-        newAnnotation.put("active", false);
-        newAnnotation.put("creating", false);
-        newAnnotation.put("dragging", false);
-        newAnnotation.put("uuid", UUID.randomUUID().toString());
-        newAnnotation.put("index", annotationsList.size());
-        newAnnotation.put("type", 2);  // 猜测2是是多边形标签
-
-        annotationsList.add(newAnnotation);
-
-        String updatedAnnotations;
-        try {
-            updatedAnnotations = objectMapper.writeValueAsString(annotationsList);
-        } catch (Exception e) {
-            return Result.error("Failed to serialize annotations");
-        }
-
-        return Result.success(updatedAnnotations);
-    }
-
-    private String generateTxtFile(List<List<Integer>> coor, String imagePath) {
-        // 假设 imagePath 是 "/path/to/image.jpg"，可能需要改一下
-        String imageName = Paths.get(imagePath).getFileName().toString();
-        String txtFileName = imageName.replaceFirst("[.][^.]+$", "") + ".txt"; // 去掉后缀
-        String txtFilePath = Paths.get(Paths.get(imagePath).getParent().toString(), txtFileName).toString();
-        
-        // 调用 Python 脚本并传递坐标
-        ProcessBuilder processBuilder = new ProcessBuilder("python", ".py脚本文件的路径", coor.get(0).toString(), coor.get(1).toString(), imagePath);
-        processBuilder.redirectErrorStream(true);
-        try {
-            Process process = processBuilder.start();
-            process.waitFor();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //py跑完了返回txt路径（看起来有点多余）
-        return txtFilePath;
-    }
-
-    /**
-     * 把txt转化为coordinates的方法，给他单独抽出来了
-     * */
-    private List<List<Integer>> readCoordinatesFromTxt(String txtFilePath) {
         List<List<Integer>> allPoints = new ArrayList<>();
         try {
-            List<String> lines = Files.readAllLines(Paths.get(txtFilePath));
-            // 读取所有点
+            List<String> lines = Files.readAllLines(Paths.get("src/main/resources/static/output_coordinates.txt"));
+            // 先读取所有点
             for (String line : lines) {
                 String[] parts = line.split(", ");
                 for (int i = 0; i < parts.length; i += 2) {
@@ -223,12 +152,49 @@ public class AnnotationController {
                     allPoints.add(point);
                 }
             }
+
+            List<List<Integer>> coordinates=choicePointCounts(allPoints,polygonsides);
+            if(coordinates==null) {
+                return Result.error("采样点数不能超过原始点数：" + allPoints.size());
+            }
+            // 解析现有的 annotation 字符串
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<Map<String, Object>> annotationsList;
+            try {
+                // 解码并解析 JSON 字符串
+                String decodedAnnotations = java.net.URLDecoder.decode(annotations, StandardCharsets.UTF_8);
+                annotationsList = objectMapper.readValue(decodedAnnotations, new TypeReference<>() {
+                });
+            } catch (Exception e) {
+                return Result.error("Failed to parse annotations");
+            }
+
+            // 创建新的 annotation
+            Map<String, Object> newAnnotation = new LinkedHashMap<>();
+            newAnnotation.put("label", "");
+            newAnnotation.put("coor", coordinates);
+            newAnnotation.put("active", false);
+            newAnnotation.put("creating", false);
+            newAnnotation.put("dragging", false);
+            newAnnotation.put("uuid", UUID.randomUUID().toString());
+            newAnnotation.put("index", annotationsList.size());
+            newAnnotation.put("type", 2);
+
+            annotationsList.add(newAnnotation);
+
+            String updatedAnnotations;
+            try {
+                updatedAnnotations = objectMapper.writeValueAsString(annotationsList);
+            } catch (Exception e) {
+                return Result.error("Failed to serialize annotations");
+            }
+
+            return Result.success(updatedAnnotations);
         } catch (Exception e) {
-            e.printStackTrace();
-            return Collections.emptyList(); // 如果读取失败，返回空列表
+            return Result.error("Failed to process coordinates: " + e.getMessage());
         }
-        return allPoints;
     }
+
 
 
     /**
@@ -254,15 +220,25 @@ public class AnnotationController {
 
     /**
      * 全自动标注
-     * 访问一个文档的所有标注，以换行符为分界符，
+     * 访问一个文档的所有标注，以换行符为分界符
      */
     @PatchMapping("/fullauto")
     public Result<?> fullautoAnnotation(@RequestBody Map<String, Object> payload) {
         String annotations = (String) payload.get("annotations");
+        Integer polygonsides = (Integer) payload.get("polygonSides");
+//        Long imageId = (Long) payload.get("imageId");
+
+        // 调用python生成坐标 需要传入path
+//        Long userId = BaseContext.getCurrentId();
+//        String filePath = userId + "/" + imageService.getImageById(imageId, userId).getPath();
+//        Path path = Paths.get(uploadDir).resolve(filePath).toAbsolutePath().normalize();
+//        String pathStr = path.toString();
+//        System.out.println("path:"+pathStr);
+
 
         try {
             // 读取所有坐标行
-            List<String> lines = Files.readAllLines(Paths.get("src/main/resources/static/output_full.txt"));
+            List<String> lines = Files.readAllLines(Paths.get("src/main/resources/static/test1.txt"));
 
             // 解析现有的 annotation 字符串
             ObjectMapper objectMapper = new ObjectMapper();
@@ -276,21 +252,27 @@ public class AnnotationController {
             }
 
             // 处理每一个多边形
-            for (int lineIndex=0;lineIndex<lines.size();lineIndex++) {
+            for (int lineIndex = 0; lineIndex < lines.size(); lineIndex++) {
                 String[] parts = lines.get(lineIndex).split(",");
+                List<List<Integer>> allPoints = new ArrayList<>();  // 存储原始点
 
-                List<List<Integer>> polygonPoints = new ArrayList<>();  // 一个多边形的所有坐标
-                for (int i = 0; i < parts.length; i+=2) {
-                    List<Integer> point = new ArrayList<>();   // 存放该行所有坐标
+                // 读取该多边形的所有原始点
+                for (int i = 0; i < parts.length; i += 2) {
+                    List<Integer> point = new ArrayList<>();
                     point.add(Integer.parseInt(parts[i]));
                     point.add(Integer.parseInt(parts[i + 1]));
-                    polygonPoints.add(point);
+                    allPoints.add(point);
+                }
+
+                List<List<Integer>> coordinates=choicePointCounts(allPoints,polygonsides);
+                if(coordinates==null) {
+                    return Result.error("采样点数不能超过原始点数：" + allPoints.size());
                 }
 
                 // 创建新的标注对象
                 Map<String, Object> newAnnotation = new LinkedHashMap<>();
-                newAnnotation.put("label", "polygons" + (lineIndex + 1));  // 编号从1开始
-                newAnnotation.put("coor", polygonPoints);
+                newAnnotation.put("label", "polygons" + (lineIndex + 1));
+                newAnnotation.put("coor", coordinates);
                 newAnnotation.put("active", false);
                 newAnnotation.put("creating", false);
                 newAnnotation.put("dragging", false);
--- src/main/java/com/wy0225/imbrlabel/method/points.java
+++ src/main/java/com/wy0225/imbrlabel/method/points.java
@@ -1,5 +1,8 @@
 package com.wy0225.imbrlabel.method;
 
+import com.wy0225.imbrlabel.pojo.Result;
+import org.apache.ibatis.jdbc.Null;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,5 +31,69 @@ public class points {
         point.add((int) Math.round(p1.get(0) + (p2.get(0) - p1.get(0)) * ratio));
         point.add((int) Math.round(p1.get(1) + (p2.get(1) - p1.get(1)) * ratio));
         return point;
+    }
+
+
+    /**
+     * 从所有的点allPoints中选取polygonsides个
+     * @param allPoints
+     * @param polygonsides
+     * @return
+     */
+    public static List<List<Integer>> choicePointCounts(List<List<Integer>> allPoints,Integer polygonsides) {
+        List<List<Integer>> coordinates=new ArrayList<>();
+        // 如果没有指定 polygonsides，使用所有点
+        if (polygonsides == null) {
+            coordinates = allPoints;
+        } else {
+            if (polygonsides > allPoints.size()) {
+                return null;
+            }
+
+            // 确保曲线闭合
+            if (!allPoints.isEmpty() && !points.isClosePoints(allPoints.get(0), allPoints.get(allPoints.size() - 1))) {
+                allPoints.add(new ArrayList<>(allPoints.get(0)));
+            }
+
+            // 计算累积距离
+            List<Double> cumulativeDistances = new ArrayList<>();
+            cumulativeDistances.add(0.0);
+            double totalDistance = 0.0;
+
+            for (int i = 1; i < allPoints.size(); i++) {
+                totalDistance += points.distance(allPoints.get(i-1), allPoints.get(i));
+                cumulativeDistances.add(totalDistance);
+            }
+
+            // 均匀采样
+            coordinates = new ArrayList<>();
+            coordinates.add(allPoints.get(0));
+
+            double step = totalDistance / (polygonsides - 1);
+            double currentDistance = step;
+            int currentIndex = 0;
+
+            for (int i = 1; i < polygonsides - 1; i++) {
+                while (currentIndex < allPoints.size() - 1 &&
+                        cumulativeDistances.get(currentIndex) < currentDistance) {
+                    currentIndex++;
+                }
+
+                if (currentIndex > 0) {
+                    List<Integer> p1 = allPoints.get(currentIndex - 1);
+                    List<Integer> p2 = allPoints.get(currentIndex);
+                    double d1 = cumulativeDistances.get(currentIndex - 1);
+                    double d2 = cumulativeDistances.get(currentIndex);
+                    double ratio = (currentDistance - d1) / (d2 - d1);
+
+                    List<Integer> interpolatedPoint = points.interpolatePoint(p1, p2, ratio);
+                    coordinates.add(interpolatedPoint);
+                }
+
+                currentDistance += step;
+            }
+            coordinates.add(allPoints.get(allPoints.size() - 1));
+        }
+        return coordinates;
     }
 }
